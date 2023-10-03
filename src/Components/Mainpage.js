3a6343704356a37876b3b5e768c5fc02a428e00d
AYO MIDE <ayomide@Ayomides-MacBook-Pro.local>
2023-10-03 17:57:42 -0400

    implementing filter option

--- src/Components/Mainpage.js
+++ src/Components/Mainpage.js
@@ -34,9 +34,12 @@ const Mainpage = () => {
 
     const userstyle = {
         display: "flex",
-        justifyContent: "space-between",
-        width: "90%",
-        margin: "1rem auto"
+        justifyContent: "space-around",
+        width: "100%",
+        alignItems: "center",
+        position: "sticky",
+        zIndex: "10",
+        backgroundColor: "white"
     }
     const handleRemoveFromCart = (index) => {
         setCart((prevCart) => {
@@ -45,7 +48,6 @@ const Mainpage = () => {
             return updatedCart;
         });
     };
-
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
@@ -60,21 +62,120 @@ const Mainpage = () => {
             });
     }, []);
 
+
+    const [selectedCategory, setSelectedCategory] = useState('All');
+    const [sortOrder, setSortOrder] = useState('asc');
+    const [itemsToDisplay, setItemsToDisplay] = useState(true);
+
+    const [searchTerm, setSearchTerm] = useState('');
+    const filterProductsByCategory = (products) => {
+        if (selectedCategory === 'All') {
+            return products;
+        }
+        return products.filter((product) => product.category === selectedCategory);
+    };
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then((response) => response.json())
+            .then((data) => {
+                const filteredProducts = filterProductsByCategory(data);
+                const sortedProducts = sortProductsByPrice(filteredProducts);
+                const searchedProducts = filterProductsBySearchTerm(sortedProducts);
+                setData(searchedProducts);
+                setItemsToDisplay(searchedProducts.length > 0);
+                setLoading(false);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            });
+    }, [selectedCategory, sortOrder, searchTerm]);
+
+    const handleCategoryChange = (e) => {
+        setSelectedCategory(e.target.value);
+    };
+
+    const sortProductsByPrice = (products) => {
+        return products.slice().sort((a, b) => {
+            const priceA = parseFloat(a.price);
+            const priceB = parseFloat(b.price);
+
+            if (sortOrder === 'asc') {
+                return priceA - priceB;
+            } else {
+                return priceB - priceA;
+            }
+        });
+    };
+    const handleSortOrderChange = (e) => {
+        setSortOrder(e.target.value);
+    };
+
+
+    const filterProductsBySearchTerm = (products) => {
+        return products.filter((product) =>
+            product.title.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+    };
+
+    const handleSearchTermChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
+
+
+
+
     return (
         <div>
+
             {loading ? (<p>Loading...</p>) : (
                 <>
+                    <div className="masterfilter">
+                        <div style={userstyle}>
+                            <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                                <p style={{ fontWeight: "bold", fontSize: "1.2rem" }}>Welcome {username}</p>
+                            </div>
+                            <Link to="/" style={{ backgroundColor: "#3c009d", color: "white", height: "fit-content", padding: ".4rem 1rem", fontWeight: "bold" }}>Sign Out</Link>
+                        </div>
+                        <div className="filterdiv">
+                            <div className='filter1'>
+                                <label htmlFor="dropdown">Filter Products by:</label>
+                                <select id="dropdown" value={selectedCategory} onChange={handleCategoryChange}>
+                                    <option value="All">All</option>
+                                    <option value="men's clothing">Men's Clothing</option>
+                                    <option value="women's clothing">Women's Clothing</option>
+                                </select>
+                            </div>
 
-                    <div style={userstyle}>
-                        <Link to="/" style={{ backgroundColor: "#3c009d", color: "white", height: "fit-content", padding: ".4rem 1rem", fontWeight: "bold" }}>Sign Out</Link>
-                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <p style={{ fontWeight: "bold", fontSize: "1.2rem" }}>Welcome {username}</p>
-                            <div style={{ backgroundColor: "#3c009d", textTransform: "capitalize", padding: "1rem", borderRadius: "50%", height: "3rem", width: "3rem", textAlign: "center" }}>
-                                <h1 style={{ fontWeight: "bolder", textAlign: "center", color: "white" }}>{username.charAt(0)}</h1>
+                            <div className='filter1'>
+                                <label htmlFor="sortOrderDropdown">Sort by Price:</label>
+                                <select id="sortOrderDropdown" value={sortOrder} onChange={handleSortOrderChange}>
+                                    <option value="asc">Lowest to Highest</option>
+                                    <option value="desc">Highest to Lowest</option>
+                                </select>
+                            </div>
+
+                            <div className='searchdiv'>
+                                <label htmlFor="searchBar">Search Product</label>
+                                <input
+                                    type="text"
+                                    id="searchBar"
+                                    value={searchTerm}
+                                    onChange={handleSearchTermChange}
+                                    placeholder="Search..."
+                                />
                             </div>
                         </div>
+
+
                     </div>
-                    <button onClick={() => setShowCart(!showCart)} style={{ marginLeft: "2rem", position: "fixed", top: "100px" }}> <div style={{ fontWeight: "bold" }}>View Cart</div>
+
+
+
+
+
+                    <button onClick={() => setShowCart(!showCart)} style={{ marginLeft: "5rem", position: "fixed", bottom: "0", zIndex: "999999999999" }}> <div style={{ fontWeight: "bold" }}>View Cart</div>
                         {showCart ? (<FaShoppingCart style={{ fontSize: "3rem" }} />) : (
                             <>
                                 <div className="cart-icon-container">
@@ -89,7 +190,7 @@ const Mainpage = () => {
                     </button>
 
                     <div className="products">
-                        {data.map((product) => (
+                        {itemsToDisplay ? (data.map((product) => (
                             <div key={product.id} className="product-card" >
                                 <div onClick={() => handleProductClick(product)} className="product-info" ><BsInfoCircle className="info-icon" /></div>
                                 <img src={product.image} alt={product.title} className="product-image" />
@@ -99,7 +200,7 @@ const Mainpage = () => {
                                     <button className='cartAddBtn' onClick={() => handleAddToCart(product)}><FaShoppingCart style={{ fontSize: "3rem" }} /></button>
                                 </div>
                             </div>
-                        ))}
+                        ))) : (<p>No items to display</p>)}
                     </div>
 
                     {selectedProduct && (
@@ -122,7 +223,7 @@ const Mainpage = () => {
                     {showCart && (
                         <div className="cart-popup">
                             <button className='popuphide' onClick={() => setShowCart(false)}>Hide Cart</button>
-                            <h2 style={{ fontWeight: "bold", fontSize: "2rem", marginTop: "2rem" }}>Total Cart:</h2>
+                            <h2 style={{ fontWeight: "bold", fontSize: "1.5rem", marginTop: "2rem" }}>{username}'s Cart:</h2>
                             <ul>
                                 {cart.map((item, index) => (
                                     <li key={index} className="sidelist">
